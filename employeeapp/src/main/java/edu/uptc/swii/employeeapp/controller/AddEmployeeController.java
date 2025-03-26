8432df531e2b438afad03b1395570b0ccf5e9730
DavidSan <84092328+DavidSan9896@users.noreply.github.com>
2025-03-25 21:39:00 -0500

    completo

--- employeeapp/src/main/java/edu/uptc/swii/employeeapp/controller/AddEmployeeController.java
+++ employeeapp/src/main/java/edu/uptc/swii/employeeapp/controller/AddEmployeeController.java
@@ -2,6 +2,7 @@ package edu.uptc.swii.employeeapp.controller;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -9,13 +10,13 @@ import edu.uptc.swii.employeeapp.model.Employee;
 import edu.uptc.swii.employeeapp.service.EmployeeDAOImpl;
 import edu.uptc.swii.employeeapp.service.IEmployeeDAO;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.ModelAndView;
 
-
-@RestController
+@RestController("/employee")
 public class AddEmployeeController {
     private IEmployeeDAO employeeDAO = new EmployeeDAOImpl();
 
@@ -28,26 +29,26 @@ public class AddEmployeeController {
             return new RedirectView("./confirm.html");
     }
 
-    @DeleteMapping("/deletebyid")
-    public RedirectView deleteEmployee(@RequestParam String id) {
+    @GetMapping("/delete/{id}")
+    public RedirectView deleteEmployee(@PathVariable("id") String id) {
         boolean res = this.employeeDAO.deleteEmployeeById(id);
+        System.out.println("Antes delet res:" + res);
         if (!res)
-            return new RedirectView("./error.html");
+            return new RedirectView("/error.html");
         else
-            return new RedirectView("./confirm.html");
+            return new RedirectView("/confirm.html");
     }
 
-    @RequestMapping("/findbyid")
-    public Employee findByID(String id){
+    @GetMapping("/findbyid/{id}")
+    public Employee findByID(@PathVariable("id") String id){
         return this.employeeDAO.findById(id);
     }
 
-    @RequestMapping("/findall")
+    @GetMapping("/findall")
     public ModelAndView findAll() {
         List<Employee> employees = this.employeeDAO.findAll();
         ModelAndView mav = new ModelAndView("viewAllEmployees");
         mav.addObject("employees", employees);
         return mav;
     }
-
 }
